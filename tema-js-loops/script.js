75153bfcaeb64c069b9c442cead5665e5a881351
condurachi13 <kickor_rocks_you@yahoo.com>
2023-08-13 21:48:24 +0300

    am adaugat primele doua cerinte

---
+++ tema-js-loops/script.js
@@ -0,0 +1,19 @@
+let numere = [7, 10, 55, 3, 104, 73];
+let suma = 0;
+for (let i = 0; i < numere.length; i++){
+    suma += numere[i];
+}
+let media = suma / numere.length;
+console.log(media)
+
+//
+
+let array = [56, "mere", false, 103, 250, "gutui"]
+let doarNumere = array.filter(Number);
+let total = 0;
+for (let i = 0; i < doarNumere.length; i++) {
+    total += doarNumere[i];
+}
+console.log(total);
+
+//
