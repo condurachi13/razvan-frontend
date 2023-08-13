316a8b64c5a0f710d727b1e96fad51910692c0c6
condurachi13 <kickor_rocks_you@yahoo.com>
2023-08-13 20:25:56 +0300

    Am adaugat si bonus, plus mici modif. la formule

--- tema-js-control-flow/script.js
+++ tema-js-control-flow/script.js
@@ -1,21 +1,33 @@
 
-let tip = prompt("Introdu tipul mijlocului de transport. ex: motocicleta, autobuz, autocar, autoturism, etc.")
+let tip = prompt("Introdu tipul mijlocului de transport. ex: motocicleta, autobuz, masina, atv, tractor")
 let cc = prompt("Introdu capacitatea cilindrica");
+let totalPlata;
+let fractiuni = cc / 200;
+let sumaTractor = Math.ceil(fractiuni) * 22;
+let sumaAutobuz = Math.ceil(fractiuni) * 28;
+let sumaMotorMic = Math.ceil(fractiuni) * 8; 
+let sumaMotocicletaMare = Math.ceil(fractiuni) * 9; 
+let sumaMotorMediu = Math.ceil(fractiuni) * 22;
+let sumaMotorMediuDoi = Math.ceil(fractiuni) * 85;
+let sumaMotorMediuTrei = Math.ceil(fractiuni) * 171;
+let sumaMotorMare = Math.ceil(fractiuni) * 345;
 function impozit(cc){
-    let totalPlata;
-    let fractiuni = cc / 200;
-    let sumaMotorMic = Math.ceil(fractiuni) * 8; 
-    let sumaMotorMediu = Math.ceil(fractiuni) * 22;
-    let sumaMotorMediuDoi = Math.ceil(fractiuni) * 85;
-    let sumaMotorMediuTrei = Math.ceil(fractiuni) * 171;
-    let sumaMotorMare = Math.ceil(fractiuni) * 345;
 
 
-
-    if (cc <= 1000){
+    if (tip === "tractor") {
+        totalPlata = sumaTractor;
+    } else if (cc <= 1600  && tip === "masina") {
         totalPlata = sumaMotorMic;
-    } else if (cc <= 1600 && cc >= 1001){
+    } else if (cc <= 1600  && tip === "motocicleta") {
         totalPlata = sumaMotorMic;
+    } else if (cc > 1600  && tip === "motocicleta") {
+        totalPlata = sumaMotocicletaMare;
+    } else if (cc <= 1600  && tip === "atv") {
+        totalPlata = sumaMotorMic;
+    } else if (cc >= 1601  && tip === "atv") {
+        totalPlata = sumaMotocicletaMare;
+    } else if (tip === "autobuz") {
+        totalPlata = sumaAutobuz;
     } else if (cc <= 2000 && cc >= 1601){
         totalPlata = sumaMotorMediu;
     } else if (cc <= 2600 && cc >= 2001){
@@ -25,12 +37,9 @@ function impozit(cc){
     } else if (cc >= 3001){
         totalPlata = sumaMotorMare;
     } else {
-        
         return alert("Nu ati introdus un numar!")
     }
     console.log(totalPlata)
      return totalPlata;
-     
 }
-alert(Math.floor(impozit(cc)) + " RON / an.");
-
+alert(Math.floor(impozit(cc)) + " RON / an pentru " + tip);
